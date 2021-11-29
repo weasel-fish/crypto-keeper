eb47de3cf333f3b7b55d0e136e950a0e3bfb60ce
Kyle Ermentrout <kyle.ermentrout@gmail.com>
2021-11-29 12:41:05 -0500

    Completed preliminary CRUD queries for users. Still need to properly customize error functionality.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,3 +14,6 @@ app.listen(port, () => {
 app.get('/', db.displayHome)
 app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
--- backend/src/queries.ts
+++ backend/src/queries.ts
@@ -1,13 +1,12 @@
 import { Request, Response, NextFunction } from 'express'
 
 const Pool = require('pg').Pool
-
 const pool = new Pool({
   user: 'kyleermentrout',
   host: 'localhost',
   database: 'crypto_keeper_dev',
 //   password: 'password',
-  port: 5432,
+  port: 5432
 })
 
 const displayHome = (req: Request, res: Response) => {
@@ -33,7 +32,57 @@ const getUserById = (req:Request, res:Response) => {
     }
     res.status(200).json(result.rows)
   })
-
 }
 
-module.exports = {getUsers, displayHome, getUserById}
+const createUser = (req:Request, res:Response) => {
+  const { name, email } = req.body
+
+  pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error: Error, result: any) => {
+    if(error) {
+      throw error //Shuts down server, revisit
+    }
+    pool.query('SELECT * FROM users WHERE email = $1', [email], (error: Error, result: any) => {
+      if(error) {
+        throw error //Shuts down server, revisit
+      }
+      res.status(201).json(result.rows[0])
+    })
+    
+  })
+}
+
+const updateUser = (req:Request, res:Response) => {
+  const id = parseInt(req.params.id)
+  const { name, email } = req.body
+
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error: Error, results: any) => {
+      if (error) {
+        throw error
+      }
+      pool.query('SELECT * FROM users WHERE id = $1', [id], (error: Error, result: any) => {
+        if(error) {
+          throw error //Shuts down server, revisit
+        }
+        let customResponse = { message: 'Updated user.', body: result.rows[0]}
+        res.status(201).json(customResponse)
+      })
+    }
+  )
+}
+
+const deleteUser = (req:Request, res:Response) => {
+  const id = parseInt(req.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error: Error, result: any) => {
+    if (error) {
+      throw error
+    }
+
+    res.status(200).send('User deleted successfully.')
+  })
+}
+
+module.exports = {getUsers, displayHome, getUserById, createUser, updateUser, deleteUser}
