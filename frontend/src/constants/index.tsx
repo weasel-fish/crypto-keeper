1ff2ea8de3d967d3d7281150c1bd602f6ce3042b
Kyle Ermentrout <kyle.ermentrout@gmail.com>
2021-12-04 21:14:05 -0500

    Use environment variable for api url

--- frontend/src/constants/index.tsx
+++ frontend/src/constants/index.tsx
@@ -1,2 +1,2 @@
-export const API_ROOT = 'http://localhost:3000'
+export const API_ROOT = process.env.REACT_APP_API_URL || 'http://localhost:3000'
 export const COIN_API_ROOT = 'https://api.exchange.coinbase.com'
