24ead504a6ae60cba00749d2a12146f49487d4e7
Kyle Ermentrout <kyle.ermentrout@gmail.com>
2021-12-04 20:54:45 -0500

    disable ssl to postgres

--- backend/src/queries.ts
+++ backend/src/queries.ts
@@ -1,15 +1,12 @@
 import { Request, Response } from 'express'
 
 let pg = require('pg');
-if (process.env.DATABASE_URL) {
-  pg.defaults.ssl = true;
-}
 
 let connString = process.env.DATABASE_URL || 'postgresql://postgres:password@localhost:localpostgresport/yourlocaldbname';
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString : connString
+  connectionString : connString,
 });
 
 const displayHome = (req: Request, res: Response) => {
